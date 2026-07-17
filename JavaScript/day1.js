63860612814684018ed01d15266ce78341216820
Omkar Thorat <ankit13868@gmail.com>
2026-07-17 13:32:52 +0530

    Initial project structure

---
+++ JavaScript/day1.js
@@ -0,0 +1,121 @@
+let company = "Tribaze Systems"
+let role = "Software Engineer Intern"
+let stipend = "5000"
+
+console.log(company)
+console.log(role)
+console.log(stipend)
+
+// Can you write a program that prints
+// Name : Ankit
+// Age : 23
+// Company : Tribaze
+// Role : Software Developer Intern
+// 💡 Hint: You can do it with multiple console.log() statements.
+
+let name = "Ankit"
+let age = 23;
+let company = "Tribaze"
+let role = "Software Developer Intern"
+
+console.log("Name :"+name)
+console.log("Age :"+age)
+console.log("Company :"+company)
+console.log("Role :"+role)
+
+
+let name = "Omkar";
+let age = 23;
+let company = "Tribaze";
+let salary = 5000;
+let isIntern = true;
+let phone = null;
+let city;
+
+console.log(name)
+console.log(typeof name)
+
+console.log(age)
+console.log(typeof age)
+
+console.log(company)
+console.log(typeof company)
+
+console.log(salary)
+console.log(typeof salary)
+
+console.log(isIntern)
+console.log(typeof isIntern)
+
+console.log(age)
+console.log(typeof age)
+
+console.log(phone)
+console.log(typeof phone)
+
+console.log(city)
+console.log(typeof city)
+
+let a=10;
+let b=20
+
+console.log(a+b)
+console.log(a-b)
+console.log(a%b)
+console.log(a*b)
+console.log(a/b);
+
+
+let age = 23 
+
+console.log(age >18)
+
+console.log(age <18)
+
+console.log(age != 18)
+
+console.log(age == 23)
+
+console.log(age === 23)
+
+let age = 15;
+
+if(age >= 18){
+    console.log("Eligible for voting");
+}
+else{
+    console.log("Not eligible");
+}
+
+let password = "12345";
+
+if(password == "12345"){
+    console.log("Login Successful");
+}
+else{
+    console.log("Wrong Password");
+}
+
+let marks = 82;
+
+if(marks >= 90){
+    console.log("A Grade");
+}
+else if(marks >= 75){
+    console.log("B Grade");
+}
+else if(marks >= 50){
+    console.log("C Grade");
+}
+else{
+    console.log("Fail");
+}
+
+
+let num = 10;
+
+for(let i = 1;i<=num ; i++){
+
+    console.log(i)
+}
+
